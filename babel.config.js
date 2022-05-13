0b6b8ffc9743cacc1029c4c3a4bdb845d10fb114
kraccoon-dev <kraccoon@kakao.com>
2022-05-13 14:58:29 +0900

    feat: path alias

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,29 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
-};
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          '^@pages(.+)': './pages/\\1',
+          '^@components(.+)': './components/\\1',
+          '^@images(.+)': './images/\\1',
+          '^@CTypes(.+)': './types/\\1',
+        },
+        extensions: [
+          '.ios.js',
+          '.android.js',
+          '.js',
+          '.jsx',
+          '.json',
+          '.tsx',
+          '.ts',
+          '.native.js',
+        ],
+      },
+    ],
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    'jest-hoist',
+  ],
+}
